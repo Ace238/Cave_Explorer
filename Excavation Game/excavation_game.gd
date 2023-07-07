3fafbdc7bec8e7725c8ad95a64a09de584865bdd
LeakyBoots <74520190+LeakyBoots@users.noreply.github.com>
2023-07-07 13:09:19 -0400

    Cleaned up mini-game script.

--- Excavation Game/excavation_game.gd
+++ Excavation Game/excavation_game.gd
@@ -1,46 +1,39 @@
 extends Node2D
 
-var counter = 10
-@onready var health = $wall_health
+var stability_count = 10
+@onready var stability = $Wall_Stability
+@onready var tile_map = $Wall_Tile_Map
 
 func _unhandled_input(event):
-	#if event is InputEventMouseButton and event.is_pressed():
+	# Check for left mouse click.
 	if Input.is_action_just_pressed("click_left"):
-#		var pos = event.position
-#		print(pos)
-		
-		var tile_map = $my_map
-		var tile_map_layer = 0
+		# Get position of cell clicked on.
 		var clicked_cell_position = tile_map.local_to_map(tile_map.get_local_mouse_position())
-#		print("POS:\t",  clicked_cell_position)
-#		print("DATA:\t", tile_map.get_cell_tile_data(0, clicked_cell_position))
-#		print("Source ID:\t", tile_map.get_cell_source_id(tile_map_layer, clicked_cell_position))
-#		print("Atlas Coords:\t", tile_map.get_cell_atlas_coords(tile_map_layer, clicked_cell_position))
-#		print("Alt Tile:\t", tile_map.get_cell_alternative_tile(tile_map_layer, clicked_cell_position))
-#		var all_tile_pos = tile_map.get_used_cells_by_id(0)
 		
+		# Get cell information from layer 0.
 		var clicked_cell_data = tile_map.get_cell_tile_data(0, clicked_cell_position)
 		var clicked_cell_depth = -1
 		
+		# Check if cell exists. 
 		if clicked_cell_data:
-			
-			if counter > 0:
-				counter = counter - 1
-				print(counter)
-				health.text = str(counter)
+			if stability_count > 0:
+				# Decrament wall stability and update label.
+				stability_count = stability_count - 1
+				stability.text = str(stability_count)
 				
+				# Decrament tile depth and update sprite.
 				clicked_cell_depth = clicked_cell_data.get_custom_data("depth")
-	#			print(clicked_cell_depth)
 				clicked_cell_depth = max(0, clicked_cell_depth - 1)
-				
 				tile_map.set_cell(0, clicked_cell_position, 0, Vector2i(clicked_cell_depth, 0), 0)	
 				
+				# Hide tile when depth is 0.
 				if clicked_cell_depth == 0:
 					tile_map.set_cell(0, clicked_cell_position)	
-			
-				if counter == 0:
+				
+				# When wall is not stable, switch to cave scene.
+				if stability_count == 0:
 					print("Wall collapsed.")
-					health.text = "Wall collapsed."
+					stability.text = "Wall collapsed."
 					
 					get_tree().change_scene_to_file("res://Cave_1/cave_1.tscn")
 
